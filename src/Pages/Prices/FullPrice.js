a219b1daccca404277196f2cf91572a7c907cfdb
spaceled@gmail.com <spaceled@gmail.com>
2022-09-26 07:47:13 +0600

    .....

--- src/Pages/Prices/FullPrice.js
+++ src/Pages/Prices/FullPrice.js
@@ -15,7 +15,88 @@ const FullPrice = () => {
         <img className="img-responsive mx-2" src={table} alt="" />
       </div>
       <div className="m-10 p-10">
-        <div className="collapse bordered ">
+        <div className="collapse bordered collapse-plus text-left uppercase mb-1">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-slate-200 text-gray-800 peer-checked:bg-secondary peer-checked:text-secondary-content">
+            Click me to show/hide content
+          </div>
+          <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+            <p>hello</p>
+          </div>
+        </div>
+        <div className="collapse bordered collapse-plus text-left uppercase mb-1">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-slate-200 text-gray-800 peer-checked:bg-secondary peer-checked:text-secondary-content">
+            Click me to show/hide content
+          </div>
+          <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+            <p>hello</p>
+          </div>
+        </div>
+        <div className="collapse bordered collapse-plus text-left uppercase mb-1">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-slate-200 text-gray-800 peer-checked:bg-secondary peer-checked:text-secondary-content">
+            Click me to show/hide content
+          </div>
+          <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+            <p>hello</p>
+          </div>
+        </div>
+        <div className="collapse bordered collapse-plus text-left uppercase mb-1">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-slate-200 text-gray-800 peer-checked:bg-secondary peer-checked:text-secondary-content">
+            Click me to show/hide content
+          </div>
+          <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+            <p>hello</p>
+          </div>
+        </div>
+        <div className="collapse bordered collapse-plus text-left uppercase mb-1">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-slate-200 text-gray-800 peer-checked:bg-secondary peer-checked:text-secondary-content">
+            Click me to show/hide content
+          </div>
+          <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+            <p>hello</p>
+          </div>
+        </div>
+        <div className="collapse bordered collapse-plus text-left uppercase mb-1">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-slate-200 text-gray-800 peer-checked:bg-secondary peer-checked:text-secondary-content">
+            Click me to show/hide content
+          </div>
+          <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+            <p>hello</p>
+          </div>
+        </div>
+        <div className="collapse bordered collapse-plus text-left uppercase mb-1">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-slate-200 text-gray-800 peer-checked:bg-secondary peer-checked:text-secondary-content">
+            Click me to show/hide content
+          </div>
+          <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+            <p>hello</p>
+          </div>
+        </div>
+        <div className="collapse bordered collapse-plus text-left uppercase mb-1">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-slate-200 text-gray-800 peer-checked:bg-secondary peer-checked:text-secondary-content">
+            Click me to show/hide content
+          </div>
+          <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+            <p>hello</p>
+          </div>
+        </div>
+        <div className="collapse bordered collapse-plus text-left uppercase mb-1">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-slate-200 text-gray-800 peer-checked:bg-secondary peer-checked:text-secondary-content">
+            Click me to show/hide content
+          </div>
+          <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+            <p>hello</p>
+          </div>
+        </div>
+        <div className="collapse bordered collapse-plus text-left uppercase mb-2 ">
           <input type="checkbox" className="peer" />
           <div className="collapse-title bg-slate-200 text-gray-800 peer-checked:bg-secondary peer-checked:text-secondary-content">
             Click me to show/hide content
